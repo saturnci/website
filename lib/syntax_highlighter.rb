ccdfc190e79efdde5ca290cf5073eb48c7b33a1a
Jason Swett <jason@benfranklinlabs.com>
2026-01-11 17:28:55 -0500

    Add filename bar feature for code blocks.

--- lib/syntax_highlighter.rb
+++ lib/syntax_highlighter.rb
@@ -1,15 +1,21 @@
 module SyntaxHighlighter
 
   def highlight_code_blocks(content)
-    content.gsub(/<pre><code class="language-(\w+)">(.*?)<\/code><\/pre>/m) do
-      language = $1
-      code = $2
+    content.gsub(/<pre(?: data-filename="([^"]*)")?><code class="language-(\w+)">(.*?)<\/code><\/pre>/m) do
+      filename = $1
+      language = $2
+      code = $3
 
       lexer = Rouge::Lexer.find(language)
       formatter = Rouge::Formatters::HTML.new(css_class: 'highlight')
 
       highlighted = formatter.format(lexer.lex(code))
-      "<pre class=\"highlight\"><code>#{highlighted}</code></pre>"
+
+      if filename
+        "<div class=\"code-block\"><div class=\"code-filename\">#{filename}</div><pre class=\"highlight\"><code>#{highlighted}</code></pre></div>"
+      else
+        "<pre class=\"highlight\"><code>#{highlighted}</code></pre>"
+      end
     end
   end
 end
