077aea2f2b8187a035e16e03563bda4e39e7b2d8
Jason Swett <jason@benfranklinlabs.com>
2026-03-14 11:57:49 -0400

    Add blog post generator script.

---
+++ lib/blog_post_generator.rb
@@ -0,0 +1,35 @@
+class BlogPostGenerator
+  attr_reader :title
+
+  def initialize(title:)
+    @title = title
+  end
+
+  def slug
+    raise "Title cannot be blank" if title.strip.empty?
+
+    title.downcase.gsub(/\s+/, "-")
+  end
+
+  def boilerplate
+    <<~HTML
+
+      <div class="container page-container">
+        <h1>#{title}</h1>
+
+        <div class="page-content">
+        </div>
+      </div>
+    HTML
+  end
+
+  def frontmatter
+    <<~YAML
+      ---
+      page_title: #{title}
+      nav: #{slug}
+      draft: true
+      ---
+    YAML
+  end
+end
---
+++ spec/blog_post_generator_spec.rb
@@ -0,0 +1,49 @@
+require_relative "../lib/blog_post_generator"
+
+RSpec.describe BlogPostGenerator do
+  describe "#slug" do
+    context "when a valid title is supplied" do
+      it "returns a slugified version of the title" do
+        generator = BlogPostGenerator.new(title: "My Great Post")
+        expect(generator.slug).to eq("my-great-post")
+      end
+    end
+
+    context "when a blank title is supplied" do
+      it "raises an error" do
+        generator = BlogPostGenerator.new(title: "")
+        expect { generator.slug }.to raise_error("Title cannot be blank")
+      end
+    end
+  end
+
+  describe "#frontmatter" do
+    it "includes the title, slug, and draft flag" do
+      generator = BlogPostGenerator.new(title: "My Great Post")
+      expected = <<~YAML
+        ---
+        page_title: My Great Post
+        nav: my-great-post
+        draft: true
+        ---
+      YAML
+      expect(generator.frontmatter).to eq(expected)
+    end
+  end
+
+  describe "#boilerplate" do
+    it "includes the title in an h1 and the page content wrapper" do
+      generator = BlogPostGenerator.new(title: "My Great Post")
+      expected = <<~HTML
+
+        <div class="container page-container">
+          <h1>My Great Post</h1>
+
+          <div class="page-content">
+          </div>
+        </div>
+      HTML
+      expect(generator.boilerplate).to eq(expected)
+    end
+  end
+end
